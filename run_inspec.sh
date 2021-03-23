eb5f18b04694a9162a55e662de3c7d27903c3620
Jeremy <valancejeremy@gmail.com>
2021-03-23 08:46:43 -0600

    Added mongo controls

--- run_inspec.sh
+++ run_inspec.sh
@@ -4,5 +4,8 @@ curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec
 
 CONTROLS=$(./filter_controls.py --stage scm)
 echo "Executing the following controls with Inspec:" $CONTROLS
+
+cd mongodb_enterprise_3.x
+
 # Run inspec
-inspec exec apache_2.4_server/controls/ --controls $CONTROLS --input-file=apache_2.4_server/inspec_inputs.yaml -t docker://7b197292b89c0ae9510ac9402ef6759cc74f4b6c6f8bb7fa35fe7e74e0b2fd1e
+inspec exec controls/ --controls $CONTROLS --input-file inspec_inputs.yaml
