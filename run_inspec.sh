51195d682eb7e4fdb5f5ebc00a791984cd056205
Jeremy <valancejeremy@gmail.com>
2021-03-23 08:51:59 -0600

    Added mongo conf

--- run_inspec.sh
+++ run_inspec.sh
@@ -2,6 +2,8 @@
 
 curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec
 
+inspec --chef-license=accept
+
 CONTROLS=$(./filter_controls.py --stage scm)
 echo "Executing the following controls with Inspec:" $CONTROLS
 
